a54897cf9a64e3ca10fc0a0e9ae1ea28342d1f6b
Mantovaniz <67487622+Mantovaniz@users.noreply.github.com>
2022-02-18 21:20:05 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,25 @@
+# paradigma orientação orientada a objetos
+# sintaxe (linguagem java)
+# convenções
+# 
+# composição: do que um objeto é feito
+# herança: quais são as superclasses e subclasses 
+# encapsulamento: não precisar que outras classes saibam o funcionamento de uma função
+# polimorfismo: basicamente no meio do programa poder escolher qual comando usar
+# 
+# tipos permitidos int, double, bool, char
+# estrutura de dados: arrays, list, map, sintaxe
+# 
+# class gato{
+#   string nome
+#   string raça
+#   string idade
+# }
+#
+# camelcase em java
+# CadastroGato    cadastroGato
+# NomeDeClasse
+#
+# nomes constantes tem TODAS as letras maiúsculas
+
+#system.out.printIn("O numero" + i + "eh par");
