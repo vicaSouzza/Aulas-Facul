722f10480cf98210456bad606e5959fb1266b142
Bea <67487622+Mantovaniz@users.noreply.github.com>
2022-10-20 20:32:28 -0300

    Update Imobiliaria.sql

--- Aulas-Facul/Aula 22.08.2022 - BD/Imobiliaria.sql
+++ Aulas-Facul/Aula 22.08.2022 - BD/Imobiliaria.sql
@@ -1,6 +1,11 @@
 CREATE DATABASE Imobiliaria;
 USE Imobiliaria;
 
+CREATE TABLE Categoria_imovel(
+cod_categoria INT PRIMARY KEY, 
+categoria VARCHAR(20) NOT NULL
+);   
+
 CREATE TABLE Imoveis(
 cod_imovel INT PRIMARY KEY,
 endereço VARCHAR (30) NOT NULL,
@@ -19,13 +24,6 @@ FOREIGN KEY (cod_categoriaFK) REFERENCES Categoria_imovel(cod_categoria)
  FOREIGN KEY (cod_imovelFK_statusImovel) REFERENCES Imoveis(cod_imovel)
 );
   
-  
-CREATE TABLE Categoria_imovel(
-cod_categoria INT PRIMARY KEY, 
-categoria VARCHAR(20) NOT NULL
-); 
-  
-  
 CREATE TABLE Info_Casa(
 cod_casa INT PRIMARY KEY,
 qtd_comodos_casa INT NOT NULL,
@@ -37,7 +35,7 @@ cod_imovelFK_casa INT NOT NULL,
 FOREIGN KEY (cod_imovelFK_casa) REFERENCES Imoveis(cod_imovel)
 );
   
-CREATE TABLE Info_AP
+CREATE TABLE Info_AP(
 cod_AP INT PRIMARY KEY,
 qtd_comodos_AP INT NOT NULL,
 qtd_suites_AP INT NOT NULL,
@@ -68,7 +66,12 @@ cod_imovelFK_terreno INT NOT NULL,
 FOREIGN KEY (cod_imovelFK_terreno) REFERENCES Imoveis(cod_imovel)
 );
  
-  
+
+CREATE TABLE Rel_imobiliaria (
+cod_rel_imobiliaria INT PRIMARY KEY,
+relacao VARCHAR(15) NOT NULL
+);
+ 
 CREATE TABLE Clientes (
 cod_cliente INT PRIMARY KEY,
 nome_cliente VARCHAR(30) NOT NULL,
@@ -83,27 +86,21 @@ cod_rel_imobiliariaFK_clientes INT NOT NULL,
 FOREIGN KEY (cod_imovelFK_clientes) REFERENCES  Rel_imobiliaria(cod_rel_imobiliaria)
 );
 
-CREATE TABLE Rel_imobiliaria (
-cod_rel_imobiliaria INT PRIMARY KEY,
-relacao VARCHAR(15) NOT NULL
+CREATE TABLE Corretor (
+cod_corretor INT PRIMARY KEY,
+nome_corretor VARCHAR(30) NOT NULL,
+porcentagem_comissao DECIMAL(3,2) NOT NULL,
+cod_clienteFK_corretor INT NOT NULL,
+FOREIGN KEY (cod_clienteFK_corretor) REFERENCES  Clientes(cod_cliente)
 );
- 
+
 CREATE TABLE Vendas(
 cod_vendas INT PRIMARY KEY,
 valor_venda DECIMAL(8,2) NOT NULL,
 valor_comissao DECIMAL(8,2) NOT NULL,
 data_venda VARCHAR (10) NOT NULL,
 cod_clienteFK_vendas INT NOT NULL,
-FOREIGN KEY (cod_imovelFK_clientes) REFERENCES  Clientes(cod_cliente),
+FOREIGN KEY (cod_clienteFK_vendas) REFERENCES  Clientes(cod_cliente),
 cod_corretor_vendas INT NOT NULL,
-FOREIGN KEY (cod_imovelFK_clientes) REFERENCES  Corretor(cod_corretor)
-);
- 
- 
-CREATE TABLE Corretor (
-cod_corretor INT PRIMARY KEY,
-nome_corretor VARCHAR(30) NOT NULL,
-porcentagem_comissao DECIMAL(3,2) NOT NULL,
-cod_vendaFK_corretor INT NOT NULL,
-FOREIGN KEY (cod_imovelFK_clientes) REFERENCES Vendas(cod_venda)
+FOREIGN KEY (cod_corretor_vendas) REFERENCES  Corretor(cod_corretor)
 );
